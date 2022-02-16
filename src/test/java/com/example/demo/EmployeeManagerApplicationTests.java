23e0f2dff20e527435af42c3db41c07061896812
Pham Tien Dung <ptdung2608@gmail.com>
2022-02-16 15:44:31 +0700

    Fail to add validate password

--- src/test/java/com/example/demo/EmployeeManagerApplicationTests.java
+++ src/test/java/com/example/demo/EmployeeManagerApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.demo;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class EmployeeManagerApplicationTests {
 
+class EmployeeManagerApplicationTests {
+	private Calculator Result=new Calculator();
 	@Test
 	void contextLoads() {
+		int a=5;
+		int b=10;
+		int Res=Result.add(a, b);
+		int expect=15;
+		assertThat(Res).isEqualTo(expect);
 	}
-
+	class Calculator{
+		int add(int num1 , int num2) {
+			return num1+num2;
+		}
+	}
 }
